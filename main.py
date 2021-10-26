22f6bad46181d66d3910aa8e15bd6324ee0a8238
JohnWickwastaken <74513576+JohnWickwastaken@users.noreply.github.com>
2021-10-26 17:07:44 +0530

    Second Commit

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import requests
 html_text = requests.get('https://www.iomfsa.im/enforcement/disqualified-directors/').text
 soup = BeautifulSoup(html_text, 'lxml')
 def function1():
-    directors = soup.find_all('section', class_='accordion-item')
-    for director in directors:
-        director_list = director.find('div', class_='rte').text
-        print(f'{director_list}')
+    directors = soup.find_all('section', class_='accordion-item')       # To pull each director from the list under the class 'accordion-item'
+    for director in directors:                                          # For each director in the list pulled before
+        director_list = director.find('div', class_='rte').text         # To remove all the html tags and place only the strings from their sections
+        print(f'{director_list}')                                       # Print each director in its format inside the loop
 
 function1()
